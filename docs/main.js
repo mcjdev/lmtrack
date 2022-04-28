8250399f7c82d7956283ca46abd16545a1a02201
Michael Johnson <aug484@me.com>
2022-04-28 14:39:50 +0000

    test 4

--- docs/main.js
+++ docs/main.js
@@ -1,3 +1,7 @@
-import * as d3 from "./d3.js"
+import * as d3 from ".d3"
 
-d3.select("#tracker").append("svg").attr("width", "500px").attr("height", "500px").attr("style", "border: 1px solid black;");
+const btn = document.getElementById("clickme");
+
+btn.addEventListener("click", function(ev) {
+    d3.select("#tracker").append("svg").attr("width", "500px").attr("height", "500px").attr("style", "border: 1px solid black;");
+});
